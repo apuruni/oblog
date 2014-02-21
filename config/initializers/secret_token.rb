0ce7f67db6f60f126107c7f88a47b9230aebb451
okon <okon@okon-mba.local>
2014-02-22 08:14:28 +0900

    new application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Oblog::Application.config.secret_key_base = '4a44fe745930637cb8c0e27594ec00e3827841be60df80359c9ff357b85936a7a9c408e4b569e784ee647b9e01facc38b4be5e39e33e2dc569095a7d17e2b4ad'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Oblog::Application.config.session_store :cookie_store, key: '_oblog_session'
